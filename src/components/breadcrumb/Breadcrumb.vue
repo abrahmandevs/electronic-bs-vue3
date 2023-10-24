bb4bf39bd56da848bee306e008d29ec6ab4ab5b6
arrahmna-dev <dra390@gmail.com>
2023-10-25 02:11:03 +0600

    update breadcrumb component

--- src/components/breadcrumb/Breadcrumb.vue
+++ src/components/breadcrumb/Breadcrumb.vue
@@ -1,5 +1,5 @@
 <script setup>
-	import { reactive, ref, defineProps } from "vue"
+	import { defineProps } from "vue"
 	import { ChevronRightIcon } from "@heroicons/vue/24/outline"
 	import { useRoute } from "vue-router"
 	const route = useRoute()
